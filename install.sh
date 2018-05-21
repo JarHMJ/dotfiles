77a48d6c3b7ddb40230383a5e3fca57441156b05
jarhm <849104648@qq.com>
2018-05-21 20:13:58 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###########################
+# This script installs the dotfiles and runs all other system configuration scripts
+# @author jarhmj 
+###########################
+
+ln -s -i ~/.dotfiles/.gitconfig ~/.gitconfig
