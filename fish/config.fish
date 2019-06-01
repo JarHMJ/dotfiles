252c2719f474ee867ad416cf5038e0c170702590
jarhmj <849104648@qq.com>
2019-06-01 22:27:49 +0800

    fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+# 安装fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+if status --is-interactive
+    # 定义缩写abbr
+    abbr --add --global gco git checkout
+end
